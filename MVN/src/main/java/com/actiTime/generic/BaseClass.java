2186b3fa6d4229c1cb4d171102ca3e0ab009c035
Pavaman <Pavaman@FFE-Pavaman-FFE167>
2024-09-01 12:37:43 +0530

    First commit

---
+++ MVN/src/main/java/com/actiTime/generic/BaseClass.java
@@ -0,0 +1,5 @@
+package com.actiTime.generic;
+
+public class BaseClass {
+
+}
---
+++ MVN/src/test/java/com/test/testscript/DemoMaven.java
@@ -0,0 +1,19 @@
+package com.test.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DemoMaven {
+	@Test
+	
+	public void mod1() {
+		Reporter.log("Mod1",true);
+		
+	}
+	
+	
+		
+	
+
+}
